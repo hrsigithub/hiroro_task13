90f6430e02f63dcdf0d9333e30d00d05b00881e1
ひろろ <hrsi.ad@gmail.com>
2021-02-24 00:42:30 +0900

    初期コミット

--- task13/ContentView.swift
+++ task13/ContentView.swift
@@ -7,10 +7,42 @@
 
 import SwiftUI
 
+struct task : Identifiable {
+    var id = UUID()
+    var fruitsName: String
+    var isDone : Bool
+
+    init(fruitsName: String, isDone: Bool) {
+        self.fruitsName = fruitsName
+        self.isDone = isDone
+    }
+}
+
 struct ContentView: View {
+
+    private var tasks: [task] = [
+        .init(fruitsName: "りんご", isDone: false),
+        .init(fruitsName: "みかん", isDone: true),
+        .init(fruitsName: "バナナ", isDone: false),
+        .init(fruitsName: "パイナップル", isDone: true),
+
+    ]
+
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            List {
+                ForEach(tasks) { task in
+                    HStack {
+                        Image(systemName: "checkmark")
+                            .foregroundColor(task.isDone ? .red : .white)
+
+                        Text(task.fruitsName)
+                        Spacer()
+                    }
+                }
+            }
+        }
     }
 }
 
